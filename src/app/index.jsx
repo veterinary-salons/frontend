7c826a69e3351ebdb5f1e50d44609c30c78885e5
Lvova Alina <lvova.aline@gmail.com>
2023-08-22 01:58:09 +0400

    fix: delete

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,23 +2,11 @@ import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import store from './store/index';
 import router from '../routes';
-import Buttons from '../ui/buttons/Button';
 
 function App() {
   return (
     <Provider store={store}>
       <RouterProvider router={router} />
-      <Buttons
-        variant='purple-filled'
-        size='small'
-        type='button'
-        active
-        loading={false}
-        shape='round'
-        isFiltered
-      >
-        Для кошек
-      </Buttons>
     </Provider>
   );
 }
