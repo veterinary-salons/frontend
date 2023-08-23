7a55aa5d68176ad77bfb39bb83d774b9a3434e8e
Polina Khvorostukhina <polinakhvorost@yandex.ru>
2023-08-23 13:28:31 +0300

    refactor: edite icons names and paths

--- src/ui/icons/paw/Paw.jsx
+++ src/ui/icons/paw/Paw.jsx
@@ -1,4 +1,4 @@
-import imgPaw from '../../../assets/images/icon/paw/Icon-paw.svg';
+import imgPaw from '../../../assets/images/icon/paw/icon-paw.svg';
 import styel from './Paw.module.scss';
 
 const Paw = () => <img className={styel.paw} src={imgPaw} alt="лапка" />;
---
+++ src/ui/icons/separateArrow/SeparateArrow.stories.jsx
@@ -0,0 +1,12 @@
+import SeparateArrow from './SeparateArrow';
+
+export default {
+  title: 'Icons/SeparateArrow',
+  component: SeparateArrow,
+};
+
+const Template = (arg) => <SeparateArrow {...arg} />;
+
+export const Default = Template.bind({});
+
+Default.args = {};
