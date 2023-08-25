74f9a46fd10b0f26f96dd2346a6c81a2bdddb0ab
Ivan Vahonin <vahoninivan1992@yandex.ru>
2023-08-26 01:38:33 +0300

    сделал ховер по макету и добавил подложку элементу в сторибук чтобы было видно полупрозрачный фон

--- src/ui/buttons/heart/Heart.stories.jsx
+++ src/ui/buttons/heart/Heart.stories.jsx
@@ -5,7 +5,11 @@ export default {
   component: Heart,
 };
 
-const Template = (arg) => <Heart {...arg} />;
+const Template = (arg) => (
+  <div style={{ backgroundColor: '#D4D4D4', width: '40px' }}>
+    <Heart {...arg} />
+  </div>
+);
 
 export const Default = Template.bind({});
 
