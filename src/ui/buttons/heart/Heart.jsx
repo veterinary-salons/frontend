0e7a29ff7051fc80a6b5c1ea1eef25e37f6ca3fb
Lvova Alina <lvova.aline@gmail.com>
2023-08-26 23:04:13 +0400

    style: rename heart button state & add docs

--- src/ui/buttons/heart/Heart.jsx
+++ src/ui/buttons/heart/Heart.jsx
@@ -2,25 +2,23 @@ import PropTypes from 'prop-types';
 import cn from 'classnames';
 import style from './Heart.module.scss';
 
-const Heart = ({ conditionHeart, onClick }) => (
+const Heart = ({ isActive, onClick }) => (
   <button
-    className={cn(style.like, conditionHeart ? style.action : null)}
+    className={cn(style.like, isActive ? style.action : null)}
     type="button"
-    onClick={() => onClick(conditionHeart)}
+    onClick={() => onClick(isActive)}
     aria-label="лайк"
   />
 );
 
 Heart.propTypes = {
-  conditionHeart: PropTypes.bool,
+  isActive: PropTypes.bool,
   onClick: PropTypes.func,
 };
 
 Heart.defaultProps = {
-  conditionHeart: false,
-  onClick: (conditionHeart) => !conditionHeart,
+  isActive: false,
+  onClick: (isActive) => !isActive,
 };
 
 export default Heart;
-
-// className={cn(style.like, { [style.action]: conditionHeart })}
--- src/ui/buttons/heart/Heart.stories.jsx
+++ src/ui/buttons/heart/Heart.stories.jsx
@@ -1,18 +1,43 @@
+import { useState } from 'react';
 import Heart from './Heart';
 
 export default {
   title: 'Buttons/Heart',
   component: Heart,
+  tags: ['autodocs'],
+  argTypes: {
+    isActive: {
+      type: Boolean,
+      description: 'Состояние активности', 
+      defaultValue: false,
+      options: [true, false],
+      control: {
+        type: 'radio'
+      }
+    }
+  },
 };
 
-const Template = (arg) => (
+const Template = (args) => (
   <div style={{ backgroundColor: '#D4D4D4', width: '40px' }}>
-    <Heart {...arg} />
+    <Heart {...args} />
   </div>
 );
 
 export const Default = Template.bind({});
+Default.args = {};
 
-Default.args = {
-  conditionHeart: false,
+// В компоненте-истории используем useState для управления состоянием
+export const WithState = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleHeartClick = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <div style={{ backgroundColor: '#D4D4D4', width: '40px' }}>
+      <Heart isActive={isActive} onClick={handleHeartClick} />
+    </div>
+  );
 };
