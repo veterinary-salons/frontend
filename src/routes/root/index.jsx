b6167dce9d4f3145ec9b0fe4a21dfdc714d347a8
Fra1zed <antonlaid@yandex.ru>
2023-08-27 23:38:52 +0300

    fix: исправлен root

--- src/routes/root/index.jsx
+++ src/routes/root/index.jsx
@@ -1,8 +1,5 @@
-import SpecialistCardList from "../../modules/main-page/cardList/SpecialistCardList";
-
 function Root() {
-  // return <h1>Здесь фронтенд сервиса заказа услуг и товаров для животных</h1>;
-  return (<SpecialistCardList />)
+  return <h1>Здесь фронтенд сервиса заказа услуг и товаров для животных</h1>;
 }
 
 export default Root;
