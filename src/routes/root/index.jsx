fe1f35c4c49cf1f4161fe4e52c3bfa78ebef1cff
Ivan Vahonin <vahoninivan1992@yandex.ru>
2023-08-30 22:29:28 +0300

    добавил кнопку на главной странице для перехода на тестовый компонент

--- src/routes/root/index.jsx
+++ src/routes/root/index.jsx
@@ -1,10 +1,12 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import classNames from 'classnames';
 import classes from './style.module.scss';
 
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import BreadCrumbs from '../../modules/breadCrumbs';
+
+import Button from '../../ui/buttons/originButton/Button';
 
 // Роуты при которых не должны отображаться крошки, добавлять по мере разработки, потом вынести в отдельный файл
 const dataPathWithoutCrumbs = ['/'];
@@ -25,6 +27,13 @@ function Root() {
         {!crumbsPath ? <BreadCrumbs className={classes.mt40} /> : null}
         <Outlet />
       </main>
+
+      {location.pathname === '/' ? (
+        <Link to="/test" style={{ margin: '50px' }}>
+          <Button size="large">Страница с тестовым компонентом</Button>
+        </Link>
+      ) : null}
+
       <Footer />
     </div>
   );
