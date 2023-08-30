1e9fa081a9b93dbfc2d57ee4fd7b2fa29c72f01d
Lvova Alina <lvova.aline@gmail.com>
2023-08-30 21:19:33 +0400

    fix: fix margins as figma

--- src/routes/root/index.jsx
+++ src/routes/root/index.jsx
@@ -1,5 +1,9 @@
+import CardGood from "../../components/CardGood";
+
 function Root() {
-  return <h1>Здесь фронтенд сервиса заказа услуг и товаров для животных</h1>;
+  return (
+    <CardGood />
+  );
 }
 
 export default Root;
