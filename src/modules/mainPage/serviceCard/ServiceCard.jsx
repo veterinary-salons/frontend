1cde71a700b9a49abfd1333d4f08225af1ae8154
Maria Tsay <mashuny2@yandex.ru>
2023-09-02 17:08:21 +0300

    fix: add classnames and disabled effect

--- src/modules/mainPage/serviceCard/ServiceCard.jsx
+++ src/modules/mainPage/serviceCard/ServiceCard.jsx
@@ -1,33 +1,41 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import style from './ServiceCard.module.scss';
+import classnames from 'classnames';
+import classes from './ServiceCard.module.scss';
 import toysImg from '../../../assets/images/images/services-card/toys.png';
 
-const ServiceCard = ({ color, title, img }) => (
-  <li className={style.card}>
-    <Link to="/" className={style.card__container}>
-      <div
-        className={`${style[`card__service-background_${color}`]} ${
-          style['card__service-background']
-        }`}
-      >
-        <img className={style.card__img} alt={title} src={img} />
+const ServiceCard = ({ color, title, img, disable}) => {
+  const getCardServiceBackground = classnames(
+    classes[`card__service-background_${color}`], 
+    classes['card__service-background'],
+    {
+      [classes[`card__service-background_${color}_disabled`]]:disable
+    }
+  )
+
+
+  return <li className={classes.card}>
+    <Link to="/" className={classes.card__container}>
+      <div className={getCardServiceBackground}>
+        <img className={classes.card__img} alt={title} src={img} />
       </div>
-      <h3 className={style.card__header}>{title}</h3>
+      <h3 className={classes.card__header}>{title}</h3>
     </Link>
-  </li>
-);
+  </li>;
+};
 
 ServiceCard.propTypes = {
   color: PropTypes.string,
   title: PropTypes.string,
   img: PropTypes.string,
+  disable: PropTypes.bool,
 };
 
 ServiceCard.defaultProps = {
   color: 'blue',
   title: 'Игрушки',
   img: toysImg,
+  disable: false,
 };
 
 export default ServiceCard;
