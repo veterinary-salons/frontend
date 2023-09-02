9ef1f9a563ac798f5915c05f2af8d5196eccb834
Maria Tsay <mashuny2@yandex.ru>
2023-09-02 17:10:36 +0300

    fix:changed root

--- src/routes/root/index.jsx
+++ src/routes/root/index.jsx
@@ -1,11 +1,5 @@
-// import ServiceCard from "../../modules/mainPage/serviceCard/ServiceCard";
-import ServiceCardList from '../../modules/mainPage/serviceCardList/ServiceCardList'
-// import SpecialistCard from '../../modules/mainPage/specialistCard/SpecialistCard'
-
 function Root() {
-  // return <h1>Здесь фронтенд сервиса заказа услуг и товаров для животных</h1>;
-  return <ServiceCardList />
-  // return <SpecialistCard />
+  <h1>Здесь фронтенд сервиса заказа услуг и товаров для животных</h1>;
 }
 
 export default Root;
