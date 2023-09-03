4a496ed9bfa941eea6aab32e93cbc3410a117196
Lvova Alina <lvova.aline@gmail.com>
2023-09-03 21:19:18 +0400

    Revert "fixes after review"
    This reverts commit a24004fad51ff03d969087c60222b699df6d7fa9.

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,26 @@
+import classes from './Main.module.scss';
+import SpecialistCardList from '../../modules/mainPage/specialistCardList/SpecialistCardList';
+import ServiceCardList from '../../modules/mainPage/serviceCardList/ServiceCardList';
+import Facts from '../../modules/mainPage/facts/Facts';
+// import Button from '../../ui/buttons/originButton/Button';
+
+function Main() {
+  return (
+    <main className={classes.main}>
+      <section className={classes.promo}>
+        <h1 className={classes.promo__title}>Ваш питомец — наша забота</h1>
+        <p className={classes.promo__text}>
+          Purrfect care&nbsp;&mdash; сервис подбора специалистов для ухода,
+          присмотра, лечения и&nbsp;воспитания ваших питомцев
+        </p>
+        <button className={classes.promo__button}>Найти специалиста</button>
+      </section>
+      <div className={classes.main__gap} />
+      <SpecialistCardList />
+      <Facts />
+      <ServiceCardList />
+    </main>
+  );
+}
+
+export default Main;
