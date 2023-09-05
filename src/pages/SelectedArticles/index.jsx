abbc95174a7f0294f91187abc4b66b397cdc171c
Polina Khvorostukhina <polinakhvorost@yandex.ru>
2023-09-06 00:40:16 +0300

    hotfix: edit notification message button

--- src/pages/SelectedArticles/index.jsx
+++ src/pages/SelectedArticles/index.jsx
@@ -1,32 +1,23 @@
 import PropTypes from 'prop-types';
-import { useNavigate } from 'react-router-dom';
 import ArticleCard from '../../components/ArticleCard/index';
 import NotificationMessage from '../../components/NotificationMessage/index';
 import classes from './style.module.scss';
 
-const SelectedArticles = ({ dataArticle }) => {
-  const navigate = useNavigate();
-
-  const handleNvigate = () => {
-    navigate('/magazine', { relative: 'path' });
-  };
-
-  return (
-    <section className={classes['selected-articles']}>
-      {dataArticle.length === 0 ? (
-        <NotificationMessage
-          imageNumber="8"
-          title="Тут пока нет интересных статей"
-          text="А вы знали, что собаки, снимающиеся в голливудских фильмах, сами подписывают свои контракты – отпечатком лапы?"
-          onClick={handleNvigate}
-          buttonText="Читать статьи"
-        />
-      ) : (
-        dataArticle.map((i) => <ArticleCard {...i} key={i.id} />)
-      )}
-    </section>
-  );
-};
+const SelectedArticles = ({ dataArticle }) => (
+  <section className={classes['selected-articles']}>
+    {dataArticle.length === 0 ? (
+      <NotificationMessage
+        imageNumber="8"
+        title="Тут пока нет интересных статей"
+        text="А вы знали, что собаки, снимающиеся в голливудских фильмах, сами подписывают свои контракты – отпечатком лапы?"
+        to="/magazine"
+        buttonText="Читать статьи"
+      />
+    ) : (
+      dataArticle.map((i) => <ArticleCard {...i} key={i.id} />)
+    )}
+  </section>
+);
 
 SelectedArticles.propTypes = {
   dataArticle: PropTypes.arrayOf(
