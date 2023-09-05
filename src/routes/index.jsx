cd38305177657b3a5429b5c76b8d6844e97cea44
Anton <antonlaid@yandex.ru>
2023-09-05 14:41:43 +0300

    fix: исправление пакета на маке

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -15,7 +15,6 @@ import ProfileServices from '../pages/ProfileServices';
 import ProfileGoods from '../pages/ProfileGoods';
 
 import { dataLinksProfile } from '../assets/constants/dataPath';
-import GoodsFilterMain from '../components/Filters/GoodsFilters/GoodsFilterMain';
 import petList from '../assets/constants/petList';
 import InputPrice from '../ui/forms/inputs/inputPrice';
 
@@ -54,10 +53,7 @@ const router = createBrowserRouter(
       <Route path="signup" element={<h2>Здесь будет вход</h2>} />
 
       {/*  Роут для проверки своих компонентов в адресной строке ввест /test */}
-      <Route
-        path="test"
-        element={<GoodsFilterMain/>}
-      />
+      <Route path="test" element={<InputPrice />} />
     </Route>,
   ),
 );
