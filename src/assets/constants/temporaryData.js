3385b69783631d48134ad34d5768b9d6116c96d2
Fra1zed <antonlaid@yandex.ru>
2023-09-05 15:08:44 +0300

    fix: решена проблема с файлами

---
+++ src/assets/constants/temporaryData.js
@@ -0,0 +1,91 @@
+const dataSelectedProducts = [
+  {
+    id: 1,
+    price: '353',
+    weightProduct: '600',
+    descriptionProduct:
+      'Сухой корм для кошек Royal Conin для Стерилизованных Индейка для взрослых',
+    reviews: 12,
+    image: 'https://i.ibb.co/3d8qZtD/Photo-with-like.png',
+    rating: '3.2',
+    counter: 0,
+    likes: true,
+  },
+  {
+    id: 2,
+    price: '353',
+    weightProduct: '600',
+    descriptionProduct:
+      'Сухой корм для кошек Pro Plan для Стерилизованных Индейка для взрослых',
+    reviews: 46,
+    image: 'https://i.ibb.co/3d8qZtD/Photo-with-like.png',
+    rating: '4.4',
+    counter: 0,
+    likes: true,
+  },
+  {
+    id: 3,
+    price: '353',
+    weightProduct: '600',
+    descriptionProduct:
+      'Сухой корм для кошек Purina One для Стерилизованных Индейка для взрослых',
+    reviews: 46,
+    image: '',
+    rating: '5.0',
+    counter: 0,
+    likes: true,
+  },
+  {
+    id: 4,
+    price: '353',
+    weightProduct: '600',
+    descriptionProduct:
+      'Сухой корм для кошек Farmina для Стерилизованных Индейка для взрослых',
+    reviews: 46,
+    image: 'https://i.ibb.co/3d8qZtD/Photo-with-like.png',
+    rating: '4.8',
+    counter: 0,
+    likes: true,
+  },
+  {
+    id: 5,
+    price: '353',
+    weightProduct: '600',
+    descriptionProduct:
+      'Сухой корм для кошек Royal Conin для Стерилизованных Индейка для взрослых',
+    reviews: 46,
+    image: 'https://i.ibb.co/3d8qZtD/Photo-with-like.png',
+    rating: '4.7',
+    counter: 0,
+    likes: true,
+  },
+  {
+    id: 6,
+    price: '353',
+    weightProduct: '600',
+    descriptionProduct:
+      'Сухой корм для кошек Alpha Pet для Стерилизованных Индейка для взрослых',
+    reviews: 46,
+    image: '',
+    rating: '4.0',
+    counter: 0,
+    likes: true,
+  },
+];
+
+const dataArticle = [
+  {
+    id: 1,
+    img: '',
+    title: 'Как подружить питомцев?',
+    isActive: true,
+  },
+  {
+    id: 2,
+    img: 'https://i.ibb.co/VMF37w6/photo-1.png',
+    title: 'Как правильно стричь когти?',
+    isActive: true,
+  },
+];
+
+export { dataSelectedProducts, dataArticle };
