2330c8662a3d8bb38e172b0f01247a19839e2565
Ivan Vahonin <vahoninivan1992@yandex.ru>
2023-09-23 18:16:15 +0300

    добавляет состояние профиля когда нет питомцев

--- src/modules/petCardList/index.jsx
+++ src/modules/petCardList/index.jsx
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
-import { useNavigate } from 'react-router-dom';
+import classNames from 'classnames';
+import { useNavigate, Link } from 'react-router-dom';
 import PetCard from './petCard';
-
+import Button from '../../ui/buttons/originButton/Button';
 import classes from './style.module.scss';
 // import { useEffect } from 'react';
 
@@ -12,25 +13,49 @@ const PetCardList = ({ petList }) => {
 
   // })
 
-  const createPetList = petList.map(({ id, src, petName, years, months }) => (
-    <PetCard
-      key={id}
-      id={id}
-      src={src}
-      title={petName}
-      years={years}
-      months={months}
-      onClick={() => navigate(`/${id}`)}
-    />
-  ));
+  const createPetList = petList
+    ? petList.map(({ id, src, petName, years, months }) => (
+        <PetCard
+          key={id}
+          id={id}
+          src={src}
+          title={petName}
+          years={years}
+          months={months}
+          onClick={() => navigate(`/${id}`)}
+        />
+      ))
+    : null;
 
   return (
     <section className={classes.pets}>
       <h2 className={classes.pets__title}>Мои питомцы</h2>
-      <div className={classes['pets__card-wrapper']}>
-        {createPetList}
-        <PetCard add />
-      </div>
+      {petList ? (
+        <div className={classes['pets__card-wrapper']}>
+          {createPetList}
+          <PetCard add />
+        </div>
+      ) : (
+        <div className={classes['pets__no-pet']}>
+          <h3 className={classes['pets__title-no-pet']}>
+            У вас пока нет питомцев
+          </h3>
+          <p className={classes['pets__subtitle-no-pet']}>
+            Но они, наверняка, есть.
+          </p>
+          <Link to="addpet">
+            <Button shape="round" size="small" variant="add" />
+          </Link>
+          <p
+            className={classNames(
+              classes['pets__subtitle-no-pet'],
+              classes['pets__subtitle-no-pet_black'],
+            )}
+          >
+            Добавить информацию
+          </p>
+        </div>
+      )}
     </section>
   );
 };
