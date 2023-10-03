f810391d73a9e349cd64dae80889abbe18e22ffa
Lvova Alina <lvova.aline@gmail.com>
2023-10-04 01:39:29 +0400

    feature: add dynamic article title to breadCrumbs

--- src/components/Article/index.jsx
+++ src/components/Article/index.jsx
@@ -1,14 +1,29 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
+import { setArticleTitle } from '../../app/store/articleTitle';
 import classes from './style.module.scss';
 import Heart from '../../ui/buttons/heart/Heart';
 import ArticleContent from '../ArticleContent';
 
-const Article = ({article, withMargins}) => {
-  const {id, title, date, link, source, imgContent, isLiked} = article;
+const Article = ({ article, withMargins }) => {
+  const { id, title, date, link, source, imgContent, isLiked } = article;
   const [isLikedArticle, setIsLikedArticle] = useState(isLiked);
+
+  const dispatch = useDispatch();
+
+  // Установка названия статьи в Redux при монтировании компонента
+  useEffect(() => {
+    dispatch(setArticleTitle(article.title));
+
+    // Функция очистки, которая будет вызвана при размонтировании компонента
+    return () => {
+      // Сбросить название статьи при размонтировании
+      dispatch(setArticleTitle(''));
+    };
+  }, [dispatch, title]);
 
   const handleClickLike = () => {
     setIsLikedArticle((state) => !state);
@@ -16,18 +31,17 @@ const Article = ({article, withMargins}) => {
 
   return (
     <section
-      className={classNames(classes.article, withMargins && classes.article__margins)}
+      className={classNames(
+        classes.article,
+        withMargins && classes.article__margins,
+      )}
     >
       <div className={classes.article__header}>
         <h1 className={classes.article__title}>{title}</h1>
         <p className={classes['article__prop-text']}>{date}</p>
         <p className={classes['article__prop-text']}>
           Источник:&nbsp;
-          <Link
-            className={classes.article__link}
-            to={source}
-            target='_blank'
-          >
+          <Link className={classes.article__link} to={source} target="_blank">
             {link}
           </Link>
         </p>
@@ -36,16 +50,13 @@ const Article = ({article, withMargins}) => {
       <ArticleContent id={id} imgContent={imgContent} />
 
       <div className={classes.article__like}>
-        <Heart
-          isActive={isLikedArticle}
-          onClick={handleClickLike}
-        />
+        <Heart isActive={isLikedArticle} onClick={handleClickLike} />
         <span className={classes['article__prop-text']}>
           {isLikedArticle ? 'Сохранено в избранном' : 'Сохранить в избранное'}
-          </span>
+        </span>
       </div>
     </section>
-  )
+  );
 };
 
 Article.propTypes = {
--- src/modules/breadCrumbs/index.jsx
+++ src/modules/breadCrumbs/index.jsx
@@ -1,14 +1,17 @@
 import PropTypes from 'prop-types';
 import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
 import classNames from 'classnames';
 import LinkCrumb from './linkCrumb';
 import classes from './style.module.scss';
-
 import { breadcrumbNameMap } from '../../assets/constants/dataPath';
 
 const BreadCrumbs = ({ className }) => {
   const location = useLocation();
   const pathnames = location.pathname.split('/').filter((x) => x);
+  const articleTitle = useSelector((state) => state.articleTitle);
+  const [crumbs, setCrumbs] = useState([]);
 
   const createCrumbs = pathnames.map((_value, index) => {
     const to = `/${pathnames.slice(0, index + 1).join('/')}`;
@@ -17,10 +20,23 @@ const BreadCrumbs = ({ className }) => {
     ) : null;
   });
 
+  useEffect(() => {
+    if (articleTitle) {
+      createCrumbs.push(
+        <LinkCrumb
+          to={location.pathname}
+          key={articleTitle}
+          title={articleTitle}
+        />,
+      );
+    }
+    setCrumbs(createCrumbs);
+  }, [articleTitle]);
+
   return (
     <ol className={classNames(className, classes['bread-crumbs'])}>
       <LinkCrumb start title="Главная" />
-      {createCrumbs}
+      {crumbs}
     </ol>
   );
 };
