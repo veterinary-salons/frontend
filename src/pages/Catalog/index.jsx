229c270c44915dabc4e043f502a628d0bf812bda
Maria Tsay <mashuny2@yandex.ru>
2023-11-13 11:33:41 +0300

    hotfix/adaptive360

--- src/pages/Catalog/index.jsx
+++ src/pages/Catalog/index.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import { Outlet, useLocation } from 'react-router-dom/dist';
 import classes from './style.module.scss';
 import SectionTitle from '../../components/SectionTitle';
+import MobileFilterMenu from '../../components/MobileFilterMenu'
 
 const arrWithoutFilters = ['/services', '/goods'];
 
@@ -12,6 +13,10 @@ const Catalog = ({ title }) => {
   return (
     <section className={classes.catalog}>
       <SectionTitle title={title} withFilter={!isWithFilter} />
+      <div className={classes.catalog__container}>
+        <div className={classes.catalog__sorting} />
+        <MobileFilterMenu />
+      </div>
       <Outlet />
     </section>
   );
